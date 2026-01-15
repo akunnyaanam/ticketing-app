d56c72bb88187ef0da64d5a2ac3b19782a7414d8
Khoirul Anam <akunnyakhoirulanam@gmail.com>
2026-01-15 23:10:14 +0700

    feat: forget the HasFactory

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -11,7 +12,8 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  */
 class Category extends Model
 {
-    use SoftDeletes;
+    /** @use HasFactory<\Database\Factories\CategoryFactory> */
+    use SoftDeletes, HasFactory;
 
     protected $fillable = [
         'name',
